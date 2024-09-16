160414b995441bf50a91a63dc2ef2324326026b8
Andherson Castro <agcl82@gmail.com>
2024-09-15 22:12:42 -0400

    test route added

--- src/routes/orders.router.js
+++ src/routes/orders.router.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { ordersModel } from "../models/orders.model.js";
+import { connect, disconnect } from "mongoose";
 
 const router = Router()
 
 router.get('/', async (req, res) => {
+    connect(process.env.MONGODB_URI, 
+        { dbName: 'orders' })
+        .then(() => console.log("Connected to DB"))
+        .catch(() => console.log("Error to connect to DB"))
     try{
         const orders = await ordersModel.find().lean();
         res.render('orders', {orders})
+        disconnect(process.env.MONGODB_URI, { dbName: 'orders' })
     } catch (error) {
         res.status(500).send("Server Error")
     }
@@ -15,6 +21,14 @@ router.get('/', async (req, res) => {
 router.get('/new_orders', async (req, res) => {
     try{
         res.render("new_orders")
+    } catch (error) {
+        res.status(500).send("Server Error")
+    }
+})
+
+router.get('/test', async (req, res) => {
+    try{
+        res.render("test")
     } catch (error) {
         res.status(500).send("Server Error")
     }
